da46179bb5029854825646567114422a95cfff00
Bruno Fuzetti Penso <bruno.penso@gmail.com>
2020-07-27 11:38:44 -0300

    commented code shared

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+//const shared = require('shared')
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
+/*
+app.get('/uuid', function (req, res) {
+  res.send(shared.generate())
+})
+*/
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
