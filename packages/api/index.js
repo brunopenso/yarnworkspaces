9090387167e7b369c2637a7ec4e080dc6ebee0a5
Edson Miguel Dewes <edson_dewes@hotmail.com>
2020-07-27 11:58:56 -0300

    Add yarn berry

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
+var shared = require('shared');
+
 var app = express();
-//const shared = require('shared')
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-/*
+
 app.get('/uuid', function (req, res) {
   res.send(shared.generate())
 })
-*/
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
